ff9da7655e927e5315cc902f74764843447e1c0f
Roland Swingler <roland.swingler@gmail.com>
2012-10-23 13:44:08 +0100

    Add dataset filtering to BatchedDatasetFilter.

--- lib/chicago/etl/batched_dataset_filter.rb
+++ lib/chicago/etl/batched_dataset_filter.rb
@@ -10,12 +10,18 @@ module Chicago
         @filter_column = :etl_batch_id
       end
 
+      def filter(dataset, etl_batch_id)
+        filters = conditions(dataset.dependant_tables, etl_batch_id).
+          inject {|a,b| a | b}
+        dataset.filter(filters)
+      end
+
       # Returns an array of condition hashes, for each of the tables that
       # is filterable using the etl_batch_id column.
-      def conditions(tables, etl_batch)
+      def conditions(tables, etl_batch_id)
         tables.
           select {|table| filterable?(table) }.
-          map {|table| {filter_column.qualify(table) => etl_batch.id} }
+          map {|table| {filter_column.qualify(table) => etl_batch_id} }
       end
 
       # Returns true if the table has an etl batch id column, and so can
