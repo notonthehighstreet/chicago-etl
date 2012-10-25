0f5682ebc82d422e3e021fd610a015619ce10a17
Roland Swingler <roland.swingler@gmail.com>
2012-10-25 17:10:26 +0100

    Add filter in etl batch to sequel dataset
    Move all sequel extensions under the sequel directory.

--- lib/chicago/etl.rb
+++ lib/chicago/etl.rb
@@ -1,13 +1,17 @@
+require 'sequel'
+
 require 'chicago/etl/key_builder'
-require 'chicago/etl/batched_dataset_filter'
 require 'chicago/etl/sink'
 require 'chicago/etl/mysql_load_file_value_transformer'
 require 'chicago/etl/buffering_insert_writer'
 require 'chicago/etl/mysql_dumpfile'
-require 'chicago/etl/dependant_tables'
-require 'chicago/etl/load_data_infile'
-require 'chicago/etl/batched_dataset_filter'
+
 require 'chicago/etl/load_dataset_builder'
+
+# Sequel Extensions
+require 'chicago/etl/sequel/filter_to_etl_batch'
+require 'chicago/etl/sequel/load_data_infile'
+require 'chicago/etl/sequel/dependant_tables'
 
 # Screens
 require 'chicago/etl/screens/column_screen'
---
+++ spec/etl/sequel/filter_to_etl_batch_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe Chicago::ETL::SequelExtensions::FilterToEtlBatch do
+  let(:batch) { stub(:batch, :id => 42) }
+
+  it "should do nothing to a table without an etl_batch_id column" do
+    TEST_DB.should_receive(:schema).with(:foo).and_return([])
+    TEST_DB[:foo].filter_to_etl_batch(batch).should == TEST_DB[:foo]
+  end
+
+  it "filters a table with an ETL batch id column" do
+    TEST_DB.should_receive(:schema).with(:foo).and_return([[:etl_batch_id, {}]])
+    TEST_DB[:foo].filter_to_etl_batch(batch).sql.
+      should include("`foo`.`etl_batch_id` = 42")
+  end
+
+  it "filters an aliased table with an ETL batch id column" do
+    TEST_DB.should_receive(:schema).with(:foo).and_return([[:etl_batch_id, {}]])
+    TEST_DB[:foo.as(:bar)].filter_to_etl_batch(batch).sql.
+      should include("`bar`.`etl_batch_id` = 42")
+  end
+
+  it "doesn't attempt to look for etl columns in nested queries" do
+    TEST_DB[TEST_DB[:foo].as(:bar)].filter_to_etl_batch(batch).sql.
+      should_not include("`bar`.`etl_batch_id` = 42")
+  end
+
+  it "filters based on joins" do
+    TEST_DB.should_receive(:schema).with(:baz).and_return([[:etl_batch_id, {}]])
+    TEST_DB.should_receive(:schema).with(:bar).and_return([])
+    TEST_DB.should_receive(:schema).with(:foo).and_return([])
+
+    TEST_DB[:foo].join(:bar).join(:baz).filter_to_etl_batch(batch).sql.
+      should include("`baz`.`etl_batch_id` = 42")
+  end
+
+  it "filters based on joined aliases" do
+    TEST_DB.should_receive(:schema).with(:bar).and_return([[:etl_batch_id, {}]])
+    TEST_DB.should_receive(:schema).with(:foo).and_return([])
+
+    TEST_DB[:foo].join(:bar.as(:baz)).filter_to_etl_batch(batch).sql.
+      should include("`baz`.`etl_batch_id` = 42")
+  end
+
+  it "applies filters to each unioned dataset" do
+    TEST_DB.should_receive(:schema).with(:bar).and_return([[:etl_batch_id, {}]])
+    TEST_DB.should_receive(:schema).with(:foo).and_return([[:etl_batch_id, {}]])
+
+    sql = TEST_DB[:foo].union(TEST_DB[:bar], :from_self => false).filter_to_etl_batch(batch).sql
+
+    sql.should include("`foo`.`etl_batch_id` = 42")
+    sql.should include("`bar`.`etl_batch_id` = 42")
+  end
+end
