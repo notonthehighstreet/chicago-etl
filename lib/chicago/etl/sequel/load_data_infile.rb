0ee9398bf2adc3dbc2350f2acff0808a3d20f6f5
Roland Swingler <roland.swingler@gmail.com>
2012-10-29 17:42:15 +0000

    Bug fix. Wrong method name being called.

--- lib/chicago/etl/sequel/load_data_infile.rb
+++ lib/chicago/etl/sequel/load_data_infile.rb
@@ -4,7 +4,7 @@ module Chicago
       module LoadDataInfile
         # Loads the CSV data columns in filepath into this dataset's table.
         def load_csv_infile(filepath, columns)
-          execute_dui(load_data_infile_sql(filepath, columns))
+          execute_dui(load_csv_infile_sql(filepath, columns))
         end
         
         def load_csv_infile_sql(filepath, columns)
