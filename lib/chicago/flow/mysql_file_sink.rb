7e6b3753260778e5780f8b50483c28c0b35218fa
Roland Swingler <roland.swingler@gmail.com>
2013-05-31 10:39:57 +0100

    MysqlFileSink now loads data into MySQL.

--- lib/chicago/flow/mysql_file_sink.rb
+++ lib/chicago/flow/mysql_file_sink.rb
@@ -1,23 +1,38 @@
+require 'sequel'
+require 'sequel/load_data_infile'
+
 module Chicago
   module Flow
-    class MysqlFileSink < PipelineEndpoint
-      def initialize(table, filepath, fields)
+    class MysqlFileSink < Sink
+      def initialize(db, target_table, filepath, fields)
         @fields = [fields].flatten
         @filepath = filepath
         @serializer = MysqlFileSerializer.new
+        @db = db
+        @target_table = target_table
       end
 
       def open
-        @csv = CSV.open(@filepath)
       end
 
       def <<(row)
-        open unless @csv
-        @csv << fields.map {|c| @serializer.serialize(row[c]) }
+        csv << fields.map {|c| @serializer.serialize(row[c]) }
       end
 
       def close
-        @csv.close
+        csv.close
+        load_file_into_table
+        File.unlink(@filepath) if File.exists?(@filepath)
+      end
+
+      private
+
+      def csv
+        @csv ||= CSV.open(@filepath)
+      end
+
+      def load_file_into_table
+        @db[@target_table].insert_ignore.load_csv_infile(@filepath, @fields)
       end
     end
   end
--- spec/mysql_file_sink_spec.rb
+++ spec/mysql_file_sink_spec.rb
@@ -1,9 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'csv'
+require 'sequel'
 
 describe MysqlFileSink do
+  let(:dataset) { mock(:dataset).as_null_object }
+  let(:db) { mock(:db, :[] => dataset, :schema => []) }
   let(:csv) { mock(:csv) }
 
+  let(:sink) {
+    described_class.new(db, :table, "test_file", [:foo])
+  }
+  
   before :each do
     CSV.stub(:open).and_return(csv)
     csv.stub(:<<)
@@ -11,20 +18,27 @@ describe MysqlFileSink do
   end
   
   it "writes specified columns to rows in a file" do
-    sink = described_class.new(:test_table, "test_file", [:foo])
     csv.should_receive(:<<).with([1])
     sink << {:foo => 1, :bar => 2}
   end
 
   it "serializes values before writing to the file" do
-    sink = described_class.new(:test_table, "test_file", [:foo])
     MysqlFileSerializer.any_instance.should_receive(:serialize).with(1).and_return(1)
     sink << {:foo => 1}
   end
 
   it "has defined fields" do
-    sink = described_class.new(:test_table, "test_file", [:foo])
     sink.should have_defined_fields
     sink.fields.should == [:foo]
+  end
+
+  it "loads the csv file into the database when closed" do
+    dataset.should_receive(:load_csv_infile).with("test_file", [:foo])
+    sink.close
+  end
+
+  it "specifies that INSERT IGNORE should be used" do
+    dataset.should_receive(:insert_ignore)
+    sink.close
   end
 end
